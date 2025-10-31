cf1995cd1b63af788ba0960ff6380d71b35cb670
adem rb <adamrabia79@gmail.com>
2025-10-31 09:36:04 +0100

    Refactor afficheTabMulti function to accept a parameter for multiplication value

--- tp4/ex1.php
+++ tp4/ex1.php
@@ -1,12 +1,24 @@
 <?php
-function afficheTabMulti()
+// function afficheTabMulti()
+// {
+//     echo "<table border='1' cellpadding='5'>";
+//     echo "<tr><th>Multiplication</th><th>Résultat</th></tr>";
+//     for ($i = 1; $i <= 10; $i++) {
+//         echo "<tr><td>4 x $i</td><td>" . (4 * $i) . "</td></tr>";
+//     }
+//     echo "</table>";
+// }
+
+// afficheTabMulti();
+function afficheTabMulti($n)
 {
     echo "<table border='1' cellpadding='5'>";
     echo "<tr><th>Multiplication</th><th>Résultat</th></tr>";
     for ($i = 1; $i <= 10; $i++) {
-        echo "<tr><td>4 x $i</td><td>" . (4 * $i) . "</td></tr>";
+        echo "<tr><td>$n x $i</td><td>" . ($n * $i) . "</td></tr>";
     }
     echo "</table>";
 }
 
-afficheTabMulti();
+// Exemple d’appel
+afficheTabMulti(4);
