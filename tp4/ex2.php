70c13389d96b1f3bc04ada7f973a7b9b234f4cd2
adem rb <adamrabia79@gmail.com>
2025-10-31 09:37:05 +0100

    Add pyramide function to display character pyramid

--- tp4/ex2.php
+++ tp4/ex2.php
@@ -4,4 +4,12 @@ function ligne($n, $c)
     return str_repeat($c, $n);
 }
 
-echo ligne(5, "*"); // *****
+echo ligne(5, "*"); 
+function pyramide($n, $c)
+{
+    for ($i = 1; $i <= $n; $i++) {
+        echo ligne($i, $c) . "<br>";
+    }
+}
+
+pyramide(3, "*");
