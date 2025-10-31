3e76f79b2127aa15ab3d56b6f5b5c0cb2fae3fc3
adem rb <adamrabia79@gmail.com>
2025-10-31 09:38:15 +0100

    Refactor pyramide function to include height validation and clean up commented code

--- tp4/ex2.php
+++ tp4/ex2.php
@@ -4,11 +4,23 @@ function ligne($n, $c)
     return str_repeat($c, $n);
 }
 
-echo ligne(5, "*"); 
+echo ligne(5, "*");
+// function pyramide($n, $c)
+// {
+//     for ($i = 1; $i <= $n; $i++) {
+//         echo ligne($i, $c) . "<br>";
+//     }
+// }
+
+// pyramide(3, "*");
 function pyramide($n, $c)
 {
-    for ($i = 1; $i <= $n; $i++) {
-        echo ligne($i, $c) . "<br>";
+    if ($n >= 10) {
+        echo "Erreur : la hauteur de la pyramide doit être inférieure à 10.";
+    } else {
+        for ($i = 1; $i <= $n; $i++) {
+            echo ligne($i, $c) . "<br>";
+        }
     }
 }
 
